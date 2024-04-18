30e75f5ad7553e55a4d157eb452bc31d9ece47b5
Pavankumar B Mudigoudra <pavankumar211200@gmail.com>
2024-04-18 09:33:42 +0000

    comparision operations

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,77 @@
+let score = 33;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log("-----------")
+
+// examp 2 
+
+let score1 = "35abc";
+let valueNumber1 = Number(score1)
+// console.log(typeof valueNumber1);
+// console.log(valueNumber1);
+// console.log("-----------")
+
+// examp 3 
+
+let score2 = null;
+// console.log(typeof score2);
+let valueNumber2 = Number(score2);
+// console.log(valueNumber2);
+// console.log("-----------")
+
+// examp 4 
+
+let score3 = undefined;
+let valueNumber3 = Number(score3)
+// console.log(typeof valueNumber3);
+// console.log(valueNumber3);
+// console.log("-----------")
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "mad";
+
+let isBooleanLogedin = Boolean(isLoggedIn)
+// console.log(isBooleanLogedin)
+// console.log("-----------")
+// any number other the 0 gives true 
+// empty string gives false
+// string givess true
+
+let someNumber = 21;
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****************operations******************
+
+let value = 5;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(3+"2"+2);
+// console.log(3+4+"5");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+//post-increment
+// let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter);
+
+//pre-increment
+// let gameCounter1 = 100;
+// ++gameCounter1;
+// console.log(gameCounter1);
