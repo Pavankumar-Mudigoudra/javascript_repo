521446817164884c217795973d7c25087b192049
Pavankumar B Mudigoudra <pavankumar211200@gmail.com>
2024-04-19 10:19:23 +0000

    numbers and math in JS

---
+++ basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // defining the decimal
+
+console.log("-------------------------------------------------");
+
+const num2 = 23.567
+// console.log(num2.toPrecision(3)); // how many digits want to display
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN')); // adds comma in terms of language
+
+// *************************** Maths ************************************ 
+
+// console.log(Math);
+// console.log(Math.abs(-5))
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+
+
+// console.log(Math.random());
+
+const max = 20;
+const min = 10;
+
+console.log(Math.floor(Math.random() *(max-min + 1) + min)); // min upto 10 max upto 20
+
+
